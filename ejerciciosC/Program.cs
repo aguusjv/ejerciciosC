ce1bae4d23f1c71246b697ed3d390f3e6dde7bdb
aguusjv <agustinvalenzuela1990@gmail.com>
2020-09-04 18:52:02 -0300

    Agregar archivos de proyecto.

---
+++ ejerciciosC/Program.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejerciciosC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {/*tarea2*/
+            /*int numero1 = 200;
+            int numero2 = 56;
+
+            int resultado = numero1 + numero2;
+            Console.WriteLine(resultado);
+            Console.ReadLine();*/
+
+            /*tarea3*/
+
+            /*int numero1 = 13;
+            int numero2 = 12;
+
+            int resultado = numero1 * numero2;
+            Console.WriteLine(resultado);
+            Console.ReadLine();*/
+
+            /*tarea4*/
+
+            /*int numero1 = 321;
+            int numero2 = 213;
+
+            int resultado = numero1 - numero2;
+            Console.WriteLine(resultado);
+            Console.ReadLine();*/
+
+            /*tarea5*/
+
+            /*int numero1 = 20;
+            int numero2 = 5;
+            int numero3 = 6;
+
+            int resultado = (numero1 + numero2) / numero3;
+            Console.WriteLine(resultado);
+            Console.ReadLine();*/
+
+            /*tarea6*/
+
+            /*int numero1 = 12;
+            int numero2 = 102;
+
+            int resultado = numero1 * numero2;
+
+            Console.WriteLine(resultado);
+            Console.ReadLine();*/
+
+            /*tarea7*/
+
+            /*int numero1 = 200;
+            int numero2 = 1111;
+
+            int resultado = numero1 + numero2;
+            Console.WriteLine(resultado);
+            Console.ReadLine();*/
+
+            /*tarea8*/
+
+            /*int numero1 = 10;
+            int numero2 = 25;
+
+            int resultado = numero1 * numero2;
+            Console.WriteLine(resultado);
+            Console.ReadLine();*/
+
+
+            /*tarea9*/
+
+            /*Console.WriteLine("Ingrese un número");
+            int numero1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ingrese otro número");
+            int numero2 = Convert.ToInt32(Console.ReadLine());
+
+            int resultado = numero1 * numero2;
+
+            Console.WriteLine("El resultado de la multiplicacion es: " + resultado);
+            Console.ReadLine();*/
+
+
+            /*tarea10*/
+
+            /*Console.WriteLine("Ingrese un número entero por favor: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine("El número " + numero + " es par!!");
+            }
+            else
+            {
+                Console.WriteLine("El número " + numero + " es impar!!");
+            }
+            Console.ReadLine();*/
+
+
+            /*tarea11*/
+
+            /*Console.WriteLine("Ingrese un numero entero: ");
+            int numero1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ingrese otro numero entero: ");
+            int numero2 = Convert.ToInt32(Console.ReadLine());
+
+            if (numero1 > numero2)
+            {
+                Console.WriteLine("El número " + numero1 + " es mayor");
+            }
+            else if (numero1 < numero2)
+            {
+                Console.WriteLine("El número " + numero2 + " es mayor");
+            }
+            else
+            {
+                Console.WriteLine("Los números son iguales");
+            }
+            Console.ReadLine();*/
+
+
+            /*tarea12*/
+
+            /*Console.WriteLine("Ingrese un numero entero: ");
+            int numero1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ingrese otro numero entero: ");
+            int numero2 = Convert.ToInt32(Console.ReadLine());
+
+            if (numero2 != 0)
+            {
+                int resultado = numero1 / numero2;
+                Console.WriteLine("El resultado es: " + resultado);
+            }
+
+            else
+            {
+                Console.WriteLine("Error: No se puede dividir por cero");
+            }
+            Console.ReadLine();*/
+
+
+            /*tarea13*/
+
+            /*Console.WriteLine("Escriba un número del 1 al 5: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+
+            switch (numero)
+            {
+                case 1:
+                    Console.WriteLine("UNO");
+                    break;
+                case 2:
+                    Console.WriteLine("DOS");
+                    break;
+                case 3:
+                    Console.WriteLine("TRES");
+                    break;
+                case 4:
+                    Console.WriteLine("CUATRO");
+                    break;
+                case 5:
+                    Console.WriteLine("CINCO");
+                    break;
+                default:
+                    Console.WriteLine("ERROR!!");
+                    break;
+            }
+            Console.ReadLine();*/
+
+
+            /*tarea14*/
+
+            /*int numero = 1;
+
+            while (numero <= 10)
+            {
+                Console.WriteLine(numero);
+                numero += 1;
+            }
+
+            Console.ReadLine();*/
+
+
+            /*tarea15*/
+
+            /*int numero = 1;
+
+            do
+            {
+                Console.WriteLine(numero);
+                numero += 1;
+            } while (numero <= 10);
+
+            Console.ReadLine();*/
+
+
+            /*tarea16*/
+
+            /*int num;
+
+            for (num = 10; num <= 20; num ++)
+            {
+                Console.WriteLine(num);
+            }
+
+            Console.ReadLine();*/
+
+
+
+
+
+
+        }
+    }
+}
